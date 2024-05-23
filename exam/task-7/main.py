c95237eeb6eb4ad89d18d20582f2587ee1443e39
Eslam Ghandour <eslam.aly.handour@codeacademylt.onmicrosoft.com>
2024-05-23 21:10:16 +0300

    Implemented sort_by_length function

--- exam/task-7/main.py
+++ exam/task-7/main.py
@@ -5,9 +5,11 @@
 # Returns:
 # - A list of strings sorted by their length.
 
+def sort_by_length(strings):
+    return sorted(strings, key=len)
 
 # Example usages:
-# print(sort_by_length(["a", "ccc", "dddd", "bb"]))  # Output: ["a", "bb", "ccc", "dddd"]
-# print(sort_by_length(["apple", "pie", "shortcake"]))  # Output: ["pie", "apple", "shortcake"]
-# print(sort_by_length(["may", "april", "september", "august"]))  # Output: ["may", "april", "august", "september"]
-# print(sort_by_length([]))  # Output: [], handling the empty list case
+print(sort_by_length(["a", "ccc", "dddd", "bb"]))  # Output: ["a", "bb", "ccc", "dddd"]
+print(sort_by_length(["apple", "pie", "shortcake"]))  # Output: ["pie", "apple", "shortcake"]
+print(sort_by_length(["may", "april", "september", "august"]))  # Output: ["may", "april", "august", "september"]
+print(sort_by_length([]))  # Output: [], handling the empty list case
