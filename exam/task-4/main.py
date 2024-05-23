bbe299ff00134a39bcec8a455282aefdd63b0477
Eslam Ghandour <eslam.aly.handour@codeacademylt.onmicrosoft.com>
2024-05-23 20:27:28 +0300

    Implemented a FizzBuzz function

--- exam/task-4/main.py
+++ exam/task-4/main.py
@@ -8,3 +8,18 @@
 # - num: An integer to be evaluated.
 # Returns:
 # - A string based on the criteria above.
+
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+
+print(fizz_buzz(7))
+print(fizz_buzz(9))
+print(fizz_buzz(10))
+print(fizz_buzz(15))
