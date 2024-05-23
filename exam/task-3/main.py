f441b6fb29222dd54b22aa358beb667da3023b96
Eslam Ghandour <eslam.aly.handour@codeacademylt.onmicrosoft.com>
2024-05-23 20:22:49 +0300

    Implemented a color_invert function

--- exam/task-3/main.py
+++ exam/task-3/main.py
@@ -4,3 +4,12 @@
 # - color: A tuple with three integers representing the RGB values of the color (each value ranges from 0 to 255).
 # Returns:
 # - A tuple representing the inverted RGB values.
+
+def color_invert(color):
+    inverted_color = tuple(255 - value for value in color)
+
+    return inverted_color
+
+color = (100, 150, 200)
+inverted_color = color_invert(color)
+print(f"The inverted color is {inverted_color}")
